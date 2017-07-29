388945dd8d55805490756309128db87e2d82c20e
Hange-Zoe <hange.zoe1@outlook.com>
2017-07-28 19:36:42 -0500

    Create singlecont of RepeatIteamsCoordinator  that accept one parameter: backgroundContext

--- iShoppingList/RepeatedItemCoordinator.swift
+++ iShoppingList/RepeatedItemCoordinator.swift
@@ -14,26 +14,39 @@ import CoreData
 
 // Idea, maybe we don't need to keep checking periodic, too much waste,
 // Better to just create a spare in another Entity with same schema + Date when to move to active Items list
-// As an item is completed, we check if it's a repeated item, if so we clone to new Entity with Execution date field. Everytime app start, we check the warehause for  'clone delivery of the day'  if not empty, we move them to active list. No other processing required.
+// As an item is completed, we check if it's a repeated item, if so we clone to new Entity with Execution date field. Everytime app start, we check the warehause for  'clone delivery of the day'  if not empty, we move them to active list. No other processing required. (Ans: Implemented)
 // This way we distribute the processing load and should have not impact on performance. 
 
 // also when we update an item to become repeated, we need to process that item and clone to new Entity (warehouse)
 // 
-// Things no clear yet. What if an item has been cloned (either in warehouse or active items) but then we update and change to no-repeat?
+// Things no clear yet. What if an item has been cloned (either in warehouse or active items) but then we update and change to no-repeat? (Ans: Implemented)
 // -- I think we need to clean up the warehouse of that item, but if item already in active items list, we should leave them alone. 
 
 
 
 class RepeatedItemsCoordinator {
 
-    var backgroundContext: NSManagedObjectContext!
-    var uuids = [String]()
+    private var backgroundContext: NSManagedObjectContext!
     
-    lazy var sharedInstance = RepeatedItemsCoordinator()
+    private static var sharedInstance: RepeatedItemsCoordinator!
     
-    private init() {}
+    private init(moc: NSManagedObjectContext) {
+        self.backgroundContext = moc
+        RepeatedItemsCoordinator.sharedInstance = self
+    }
+    
+    static func shared(backgroundContext: NSManagedObjectContext) -> RepeatedItemsCoordinator {
+        switch (sharedInstance, backgroundContext) {
+        case let (nil, moc):
+            sharedInstance = RepeatedItemsCoordinator(moc: moc)
+            return sharedInstance
+        default:
+            return sharedInstance
+        }
+    }
 
     // Check the warehouse for item to be delivered today 
+    
     
     func getStartAndEndOfDay() -> (startOfDay: NSDate, endOfDay: NSDate) {
         let calendar = Calendar.current
